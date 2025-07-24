2f0ae290f5794b12d33a9d3924f9f08f147a7e5c
euneun <sacroo@naver.com>
2025-07-24 23:07:52 +0900

    CRUD 테스트 완성

--- src/app/pages/nomal/page.tsx
+++ src/app/pages/nomal/page.tsx
@@ -1,21 +1,63 @@
 "use client";
 
 import { Pagination } from "@mui/material";
+import Link from "next/link";
+import { useEffect, useState } from "react";
 export default function Nomal() {
+  const [list, setList] = useState([
+    {
+      articleId: "1",
+      name: "sample",
+      title: "test",
+      contents: "test",
+      writeTime: "Wed, 21 Oct 2015 18:27:50 GMT",
+      like: 10,
+    },
+  ]);
+
+  // 데이터 불러오기
+  async function request() {
+    const response = await fetch("http://localhost:4000/board", {
+      method: "GET",
+    });
+    const data = await response.json();
+    setList(data);
+  }
+
+  useEffect(() => {
+    request();
+  }, []);
+
   return (
-    <div className="board">
-      <ol>
-        <li>
-          <ol className="list">
-            <li>No</li>
-            <li>제목</li>
-            <li>글쓴이</li>
-            <li>작성시간</li>
-            <li>조회수</li>
-          </ol>
-        </li>
-      </ol>
+    <>
+      <div className="board">
+        <ol>
+          <li>
+            <ol className="list">
+              <li>No</li>
+              <li>제목</li>
+              <li>글쓴이</li>
+              <li>추천수</li>
+              <li>글쓴시각</li>
+            </ol>
+          </li>
+          {list.map((item, index) => (
+            <li key={index + item.articleId}>
+              <ol className="list">
+                <li>{item.articleId}</li>
+                <li>{item.title}</li>
+                <li>{item.name}</li>
+                <li>{item.like}</li>
+                <li>{item.writeTime}</li>
+              </ol>
+            </li>
+          ))}
+          <li>
+            <Link href={"nomal/write"}>글쓰기</Link>
+          </li>
+        </ol>
+      </div>
       <Pagination count={10} color="secondary" />
-    </div>
+    </>
   );
 }
